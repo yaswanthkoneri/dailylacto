a7cc1335db3f4c82de3966b97c494778a7c2a959
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-10-15 09:42:31 +0530

    added back

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() title;
+  @Input() showBackButton = false;
+  @Input() showSubHeader = false;
+
+  constructor(
+    private location: Location
+  ) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
